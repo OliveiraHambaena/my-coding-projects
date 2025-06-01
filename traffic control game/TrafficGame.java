034c618324b1b4e55ce9d06e3ddc9bba2887760e
Oliveira Hambaena <167088414+OliveiraHambaena@users.noreply.github.com>
2025-06-01 04:44:56 +0200

    comments

--- traffic control game/TrafficGame.java
+++ traffic control game/TrafficGame.java
@@ -132,7 +132,7 @@ public class TrafficGame extends JPanel implements ActionListener, KeyListener {
         obstacles.clear();
         obstacleSpeed = 5; // Reset obstacle speed
         spawnChance = 100; // Reset spawn chance
-        lives = 3; // Reset
+        lives = 3; // Reset lives
         isUnstoppable = false; // Reset unstoppable mode
 
         isGamePaused = false; // Reset paused state
